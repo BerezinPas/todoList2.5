053eb1bbe0c87e3a4ca8acfd861a72fa81ee6707
Berezin <pas.1999@mail.ru>
2025-03-05 16:00:21 +0700

    fix sort

--- src/utils/sortHandler.js
+++ src/utils/sortHandler.js
@@ -1,9 +1,9 @@
 export const sortHandler = (a = '', b = '') => {
 	let i = 0;
 	while (a.length > i && b.length > i) {
-		if (a[i].toLowerCase() > b[i].toLowerCase()) {
+		if (a[i] > b[i]) {
 			return 1;
-		} else if (a[i].toLowerCase() < b[i].toLowerCase()) {
+		} else if (a[i] < b[i]) {
 			return -1;
 		}
 		i++;
